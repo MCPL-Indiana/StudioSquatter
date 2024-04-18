121059b72e515fe658e1361b10137f6a52ce2c69
Codymullis <69050280+Codymullis@users.noreply.github.com>
2024-04-18 12:14:41 -0400

    Initial Commit

---
+++ StudioSquatter/StudioSquatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioSquatterApp.swift
+//  StudioSquatter
+//
+//  Created by Cody Mullis on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct StudioSquatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
