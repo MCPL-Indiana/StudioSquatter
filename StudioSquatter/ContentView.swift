43a45516750f48407fffba9bf4639d4354ff92e4
Codymullis <69050280+Codymullis@users.noreply.github.com>
2024-04-21 17:16:44 -0400

    SSH working, added Custom Warning and Boot button for 1 studio

--- StudioSquatter/ContentView.swift
+++ StudioSquatter/ContentView.swift
@@ -27,16 +27,52 @@ struct ContentView: View {
                     .frame(height: 50)
                     .overlay {TextEditor(text: $strWarn1)
                     }
+                Button("Send", systemImage: ("paperplane.circle.fill"), action: {
+                    message1()
+                })
             }
             .padding([.top, .trailing, .leading], 5)
+            
             GridRow {
-            }
+                Button(action: boot1) {
+                    Image(systemName:"figure.kickboxing")
+                        .foregroundColor(.red)
+                    Text("Boot")
+                        .foregroundColor(.red)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
                 .padding([.bottom, .trailing, .leading], 20)
+                .controlSize(.large)
+                .frame(alignment: .center)
+            }
         }
     }
+    
+    func message1(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"" + strWarn1 + "\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        strWarn1 = ""
+        
+    }
+    
+    func boot1(){
+        let arg = ["root@10.10.12.43", "shutdown -h NOW", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        
+    }
 }
+
+
 
 #Preview {
     ContentView()
         .frame(minWidth: 350, minHeight: 350)
 }
+
