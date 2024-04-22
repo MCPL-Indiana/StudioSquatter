34cb3f030ecf792b831881d5b3519432a3b85793
Codymullis <69050280+Codymullis@users.noreply.github.com>
2024-04-21 22:35:39 -0400

    Added Studio 2, Added Icon

--- StudioSquatter/ContentView.swift
+++ StudioSquatter/ContentView.swift
@@ -12,13 +12,17 @@ struct Audio1 {
 
 struct ContentView: View {
     @State private var strWarn1: String = ""
+    @State private var strWarn2: String = ""
     var body: some View {
-        Grid {
-            GridRow {
+        VStack {
+            Text("Audio Studio 1")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+            HStack {
                 Image(systemName: "exclamationmark.bubble.fill")
                     .imageScale(.large)
                     .foregroundStyle(.tint)
-                Text("Warning \n Message")
+                Text("Send \n Message")
                     .multilineTextAlignment(.center)
                 TextField("Send Message", text: $strWarn1, axis: .vertical)
                     .textFieldStyle(.roundedBorder)
@@ -30,22 +34,127 @@ struct ContentView: View {
                 Button("Send", systemImage: ("paperplane.circle.fill"), action: {
                     message1()
                 })
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .controlSize(.extraLarge)
             }
-            .padding([.top, .trailing, .leading], 5)
+            .padding([.top, .trailing, .leading], 25)
             
-            GridRow {
-                Button(action: boot1) {
-                    Image(systemName:"figure.kickboxing")
-                        .foregroundColor(.red)
-                    Text("Boot")
-                        .foregroundColor(.red)
+            HStack {
+                Spacer()
+                
+                Button(action: warn1A) {
+                    Image(systemName:"15.circle.fill")
+                        .foregroundColor(.white)
+                        .controlSize(.extraLarge)
+                    Text("Warning")
+                        .foregroundColor(.white)
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 }
-                .padding([.bottom, .trailing, .leading], 20)
-                .controlSize(.large)
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
                 .frame(alignment: .center)
+                Spacer()
+                Button(action: warn1B) {
+                    Image(systemName:"5.circle.fill")
+                        .foregroundColor(.black)
+                        .controlSize(.extraLarge)
+                    Text("Warning")
+                        .foregroundColor(.black)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.yellow)
+                .frame(alignment: .center)
+                Spacer()
+                Button(action: boot1) {
+                    Image(systemName:"figure.kickboxing")
+                        .foregroundColor(.white)
+                    Text("Boot")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .frame(alignment: .center)
+                Spacer()
             }
+            .padding([.top, .trailing, .leading, .bottom], 25)
+            Divider()
+            Text("Audio Studio 2")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "exclamationmark.bubble.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Send \n Message")
+                    .multilineTextAlignment(.center)
+                TextField("Send Message", text: $strWarn2, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(3...3)
+                    .frame(height: 50)
+                    .overlay {TextEditor(text: $strWarn2)
+                    }
+                Button("Send", systemImage: ("paperplane.circle.fill"), action: {
+                    message2()
+                })
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .controlSize(.extraLarge)
+            }
+            .padding([.top, .trailing, .leading], 25)
+            
+            HStack {
+                Spacer()
+                
+                Button(action: warn2A) {
+                    Image(systemName:"15.circle.fill")
+                        .foregroundColor(.white)
+                        .controlSize(.extraLarge)
+                    Text("Warning")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                .frame(alignment: .center)
+                Spacer()
+                Button(action: warn2B) {
+                    Image(systemName:"5.circle.fill")
+                        .foregroundColor(.black)
+                        .controlSize(.extraLarge)
+                    Text("Warning")
+                        .foregroundColor(.black)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.yellow)
+                .frame(alignment: .center)
+                Spacer()
+                Button(action: boot2) {
+                    Image(systemName:"figure.kickboxing")
+                        .foregroundColor(.white)
+                    Text("Boot")
+                        .foregroundColor(.white)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .controlSize(.extraLarge)
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .frame(alignment: .center)
+                Spacer()
+            }
+            .padding([.top, .bottom, .trailing, .leading], 25)
+            
         }
+        .frame(minWidth: 350, minHeight: 450)
     }
     
     func message1(){
@@ -59,7 +168,68 @@ struct ContentView: View {
         
     }
     
+    func warn1A(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"You have 15 minutes left in your studio session\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        
+    }
+    
+    func warn1B(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"You have 5 minutes left in your studio session. \n Please save your work now.\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        
+    }
+    
     func boot1(){
+        let arg = ["root@10.10.12.43", "shutdown -h NOW", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        
+    }
+    
+    
+    func message2(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"" + strWarn2 + "\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        strWarn2 = ""
+        
+    }
+    
+    func warn2A(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"You have 15 minutes left in your studio session\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        
+    }
+    
+    func warn2B(){
+        let arg = ["root@10.10.12.43", "osascript -e 'tell app \"System Events\" to display dialog \"You have 5 minutes left in your studio session. \n Please save your work now.\"'", "exit"]
+        let task = Process()
+        task.launchPath = "/usr/bin/ssh"
+        task.arguments = arg
+        task.launch()
+        //task.waitUntilExit()
+        
+    }
+    
+    func boot2(){
         let arg = ["root@10.10.12.43", "shutdown -h NOW", "exit"]
         let task = Process()
         task.launchPath = "/usr/bin/ssh"
@@ -69,10 +239,8 @@ struct ContentView: View {
     }
 }
 
-
-
 #Preview {
     ContentView()
-        .frame(minWidth: 350, minHeight: 350)
+        .frame(minWidth: 350, minHeight: 450)
 }
 
